82ec787f6fcc15425645d0bc58f3b63d1b5de631
Yaniv Steinberg <yaniv.2.steinberg@tuni.fi>
2022-03-03 16:54:27 +0200

    Install Router, MUI / Add routes, HomeView

---
+++ src/views/HomeView.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { Stack } from '@mui/material';
+
+class HomeView extends React.Component {
+  render() {
+
+    return (
+
+      <Stack>
+        <h1>
+          20 Greatest Recipies for Your Army to March On
+        </h1>
+      </Stack>
+    );
+  }
+}
+
+export default HomeView;
