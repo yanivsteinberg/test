05b64aca39b01f63f8f3736357437ea832be0a29
Yaniv Steinberg <yaniv.2.steinberg@tuni.fi>
2022-03-07 23:58:30 +0200

    CardComp: Add CardActionArea and link

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -14,6 +14,8 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { CardActionArea } from '@mui/material';
+import { Link } from 'react-router-dom'
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -33,8 +35,10 @@ export default function CardComponent() {
     setExpanded(!expanded);
   };
 
+
   return (
     <Card sx={{ maxWidth: 345 }}>
+
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -49,19 +53,24 @@ export default function CardComponent() {
         title="Shrimp and Chorizo Paella"
         subheader="September 14, 2016"
       />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
+      <CardActionArea>
+        <Link to={'../views/RecipeView'} style={{ textDecoration: 'none' }}>
+          <CardMedia
+            component="img"
+            height="194"
+            image="/static/images/cards/paella.jpg"
+            alt="Paella dish"
+            style={{ paddingLeft: 13 }}
+          />
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              This impressive paella is a perfect party dish and a fun meal to cook
+              together with your guests. Add 1 cup of frozen peas along with the mussels,
+              if you like.
+            </Typography>
+          </CardContent>
+        </Link>
+      </CardActionArea>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
@@ -107,6 +116,7 @@ export default function CardComponent() {
           </Typography>
         </CardContent>
       </Collapse>
-    </Card>
+
+    </Card >
   );
 }
