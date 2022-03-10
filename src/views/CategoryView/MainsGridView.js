f940b6e8d2256c89dfaa66ee5b1691ac0ebb1de2
Hoan Bui <huuhoan.bui@tuni.fi>
2022-03-10 23:34:11 +0200

    Add more CardComponent

--- src/views/CategoryView/MainsGridView.js
+++ src/views/CategoryView/MainsGridView.js
@@ -1,25 +1,27 @@
 import * as React from "react";
-import { Grid } from '@mui/material';
-import CardComponent from '../../components/CardComponents/CardComponent';
-
+import { Grid } from "@mui/material";
+import CardComponent from "../../components/CardComponents/CardComponent";
+import CardComponent5 from "../../components/CardComponents/CardComponent5";
 class MainsGridView extends React.Component {
   render() {
-
     return (
       <>
         <div>
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
             <Grid item xs={6}>
-              <CardComponent
-              />
-
+              <CardComponent />
             </Grid>
-
+            <Grid item xs={6}>
+              <CardComponent5 />
+            </Grid>
           </Grid>
         </div>
       </>
     );
-
   }
 }
 
--- src/views/CategoryView/StartersGridView.js
+++ src/views/CategoryView/StartersGridView.js
@@ -1,25 +1,27 @@
 import * as React from "react";
-import { Grid } from '@mui/material';
-import CardComponent2 from '../../components/CardComponents/CardComponent2';
-
+import { Grid } from "@mui/material";
+import CardComponent2 from "../../components/CardComponents/CardComponent2";
+import CardComponent6 from "../../components/CardComponents/CardComponent6";
 class StartersGridView extends React.Component {
   render() {
-
     return (
       <>
         <div>
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
             <Grid item xs={6}>
-              <CardComponent2
-              />
-
+              <CardComponent2 />
             </Grid>
-
+            <Grid item xs={6}>
+              <CardComponent6 />
+            </Grid>
           </Grid>
         </div>
       </>
     );
-
   }
 }
 
