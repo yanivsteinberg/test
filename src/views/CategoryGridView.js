982531d171070a5559c6ee161e6813a00d0de47d
Hoan Bui <huuhoan.bui@tuni.fi>
2022-03-11 00:07:00 +0200

    AboutView.js CategoryGridView.js CategoryView.js HomeView.js: Add

--- src/views/CategoryGridView.js
+++ src/views/CategoryGridView.js
@@ -1,39 +1,36 @@
 import * as React from "react";
-import { Grid } from '@mui/material';
-import StartersComponent from '../components/CategoryComponents/StartersComponent';
-import MainComponent from '../components/CategoryComponents/MainComponent';
-import DrinksComponent from '../components/CategoryComponents/DrinksComponent';
-import DessertComponent from '../components/CategoryComponents/DessertComponent';
+import { Grid } from "@mui/material";
+import StartersComponent from "../components/CategoryComponents/StartersComponent";
+import MainComponent from "../components/CategoryComponents/MainComponent";
+import DrinksComponent from "../components/CategoryComponents/DrinksComponent";
+import DessertComponent from "../components/CategoryComponents/DessertComponent";
 
 class CategoryGridView extends React.Component {
   render() {
-
     return (
       <>
         <div>
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
             <Grid item xs={6}>
-              <StartersComponent
-              />
-
+              <StartersComponent />
             </Grid>
             <Grid item xs={6}>
-              <MainComponent
-              />
+              <MainComponent />
             </Grid>
             <Grid item xs={6}>
-              <DessertComponent
-              />
+              <DessertComponent />
             </Grid>
             <Grid item xs={6}>
-              <DrinksComponent
-              />
+              <DrinksComponent />
             </Grid>
           </Grid>
         </div>
       </>
     );
-
   }
 }
 
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,17 +1,13 @@
 import * as React from "react";
-import { Stack } from '@mui/material';
+import { Stack } from "@mui/material";
 
 class HomeView extends React.Component {
   render() {
-
     return (
       <div>
         <Stack>
-          <h1>
-            Recipes
-          </h1>
+          <h1>Recipes</h1>
         </Stack>
-
       </div>
     );
   }
