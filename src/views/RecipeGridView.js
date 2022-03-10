9cd40d1e937a3d78a053e7d3100ecd623f81ce89
Hoan Bui <huuhoan.bui@tuni.fi>
2022-03-10 20:14:09 +0200

    RecipeGridView.js:<Grid item>: Add <CardComponent>

--- src/views/RecipeGridView.js
+++ src/views/RecipeGridView.js
@@ -1,38 +1,36 @@
 import * as React from "react";
-import { Grid } from '@mui/material';
-import CardComponent from '../components/CardComponents/CardComponent';
-import CardComponent2 from '../components/CardComponents/CardComponent2';
-
+import { Grid } from "@mui/material";
+import CardComponent from "../components/CardComponents/CardComponent";
+import CardComponent2 from "../components/CardComponents/CardComponent2";
+import CardComponent3 from "../components/CardComponents/CardComponent3";
+import CardComponent4 from "../components/CardComponents/CardComponent4";
 
 class RecipeGridView extends React.Component {
   render() {
-
     return (
       <>
         <div>
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
             <Grid item xs={6}>
-              <CardComponent
-              />
-
+              <CardComponent />
             </Grid>
             <Grid item xs={6}>
-              <CardComponent2
-              />
+              <CardComponent2 />
             </Grid>
             <Grid item xs={6}>
-              <CardComponent
-              />
+              <CardComponent3 />
             </Grid>
             <Grid item xs={6}>
-              <CardComponent
-              />
+              <CardComponent4 />
             </Grid>
           </Grid>
         </div>
       </>
     );
-
   }
 }
 
