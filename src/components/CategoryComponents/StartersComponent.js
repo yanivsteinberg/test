96d378ef582ac9abc38a8c904a9ea30eb247d8ab
Hoan Bui <huuhoan.bui@tuni.fi>
2022-03-10 20:12:29 +0200

    StartersComponent.js: Modify content

--- src/components/CategoryComponents/StartersComponent.js
+++ src/components/CategoryComponents/StartersComponent.js
@@ -1,22 +1,21 @@
 import * as React from "react";
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';;
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { CardActionArea } from '@mui/material';
-import { Link } from 'react-router-dom'
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { CardActionArea } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function StartersComponent() {
   const [expanded, setExpanded] = React.useState(false);
 
   return (
     <Card sx={{ maxWidth: 345 }}>
-
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -32,7 +31,7 @@ export default function StartersComponent() {
         subheader="September 14, 2016"
       />
       <CardActionArea>
-        <Link to={"/recipeview1"} style={{ textDecoration: 'none' }}>
+        <Link to={"/recipeview1"} style={{ textDecoration: "none" }}>
           <CardMedia
             component="img"
             height="194"
@@ -42,14 +41,12 @@ export default function StartersComponent() {
           />
           <CardContent>
             <Typography variant="body2" color="text.secondary">
-              This impressive paella is a perfect party dish and a fun meal to cook
-              together with your guests. Add 1 cup of frozen peas along with the mussels,
-              if you like.
+              Starters consist of a wide variety of hot or cold dishes served
+              before the main course of a meal.
             </Typography>
           </CardContent>
         </Link>
       </CardActionArea>
-
-    </Card >
+    </Card>
   );
 }
