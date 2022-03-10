b0a261963a8947130469a4c534ec043a66c437da
Hoan Bui <huuhoan.bui@tuni.fi>
2022-03-10 20:11:54 +0200

    MainComponent.js: Modify content

--- src/components/CategoryComponents/MainComponent.js
+++ src/components/CategoryComponents/MainComponent.js
@@ -1,24 +1,23 @@
 import * as React from "react";
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
 
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { CardActionArea } from '@mui/material';
-import { Link } from 'react-router-dom'
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { CardActionArea } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function MainComponent() {
   const [expanded, setExpanded] = React.useState(false);
 
   return (
     <Card sx={{ maxWidth: 345 }}>
-
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -34,7 +33,7 @@ export default function MainComponent() {
         subheader="September 14, 2016"
       />
       <CardActionArea>
-        <Link to={"/recipeview1"} style={{ textDecoration: 'none' }}>
+        <Link to={"/recipeview1"} style={{ textDecoration: "none" }}>
           <CardMedia
             component="img"
             height="194"
@@ -44,14 +43,12 @@ export default function MainComponent() {
           />
           <CardContent>
             <Typography variant="body2" color="text.secondary">
-              This impressive paella is a perfect party dish and a fun meal to cook
-              together with your guests. Add 1 cup of frozen peas along with the mussels,
-              if you like.
+              A main course is the featured or primary dish in a meal consisting
+              of several courses. It usually follows the entrée course.
             </Typography>
           </CardContent>
         </Link>
       </CardActionArea>
-
-    </Card >
+    </Card>
   );
 }
