4d58fd063cbd8526286aef6c26a296cdc8057956
Hoan Bui <huuhoan.bui@tuni.fi>
2022-03-10 20:11:14 +0200

    DrinksComponent.js: Modify content

--- src/components/CategoryComponents/DrinksComponent.js
+++ src/components/CategoryComponents/DrinksComponent.js
@@ -1,23 +1,22 @@
 import * as React from "react";
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { CardActionArea } from '@mui/material';
-import { Link } from 'react-router-dom'
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { CardActionArea } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function DrinksComponent() {
   const [expanded, setExpanded] = React.useState(false);
 
   return (
     <Card sx={{ maxWidth: 345 }}>
-
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -29,11 +28,11 @@ export default function DrinksComponent() {
             <MoreVertIcon />
           </IconButton>
         }
-        title="Starters"
+        title="Drinks"
         subheader="September 14, 2016"
       />
       <CardActionArea>
-        <Link to={"/recipeview1"} style={{ textDecoration: 'none' }}>
+        <Link to={"/recipeview1"} style={{ textDecoration: "none" }}>
           <CardMedia
             component="img"
             height="194"
@@ -43,14 +42,11 @@ export default function DrinksComponent() {
           />
           <CardContent>
             <Typography variant="body2" color="text.secondary">
-              This impressive paella is a perfect party dish and a fun meal to cook
-              together with your guests. Add 1 cup of frozen peas along with the mussels,
-              if you like.
+              Fancy for a drink? ;)
             </Typography>
           </CardContent>
         </Link>
       </CardActionArea>
-
-    </Card >
+    </Card>
   );
 }
